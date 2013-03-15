4de4c9d5686252a8c0d397c80a1572c5addc9728
greemin <karl@jotech.net>
2013-03-15 18:13:40 +0100

    adding exercises

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("'C' is awesome.");
+	puts("So lets not wast any more time.");
+	puts("And move on to the next exercise.");
+	puts("ALONZI!");
+  
+	return 0;
+}
